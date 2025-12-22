da0f97268983aa069a28a8c92c3f58044f334acb
Roman Meinl <roman-meinl@ukr.net>
2025-12-22 11:13:10 -0200

    22.12 Part 3  Slider done

--- src/components/Slider/Slider.tsx
+++ src/components/Slider/Slider.tsx
@@ -1,13 +1,33 @@
+"use client";
+
 import SlideOne from "./SlideOne"
 import SlideSecond from "./SlideSecond"
-
+import {motion} from "framer-motion"
 
 
 const Slider = () => {
+
+   const slides = [
+    <SlideOne key="slide1"/>,
+    <SlideSecond key="slide2"/>
+   ]
   return (
-    <div>
-        <SlideOne/>
-        <SlideSecond/>
+    <div className="relative h-20 md:h-40 xl:h-50 w-full mb-10 md:mb-15 xl:mb-20">
+      {slides.map((slide , index) => (
+        <motion.div key={`slide-${index}`}
+        initial={{opacity:0}}
+        animate={{opacity:[0,1,1,0]}}
+        transition={{
+          duration:5,
+          repeat:Infinity,
+          repeatDelay: slides.length * 5 - 5,
+          delay: index * 5
+        }}
+        className="absolute w-full h-full"
+        >
+         {slide}
+        </motion.div>
+      ))}  
     </div>
   )
 }
