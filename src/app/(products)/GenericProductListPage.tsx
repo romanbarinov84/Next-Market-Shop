5a9e748ff4c89b655b0fb50f0048629feba58c08
Roman Meinl <roman-meinl@ukr.net>
2026-01-11 15:16:30 +0200

    11.01 les 14 (19-38)

--- src/app/(products)/GenericProductListPage.tsx
+++ src/app/(products)/GenericProductListPage.tsx
@@ -1,4 +1,3 @@
-
 import { GenericListPageProps } from '@/src/types/GenericProductListPage';
 import ProductsSection from './ProductsSection';
 import { CONFIG } from '@/config/config';
@@ -10,24 +9,28 @@ const GenericProductListPage = async ({
     searchParams: Promise<{ page?: string; itemsPerPage?: string }>;
     props: GenericListPageProps;
 }) => {
-
     const params = await searchParams;
     const page = params?.page;
     const itemsPerPage = params?.itemsPerPage || CONFIG.ITEMS_PER_PAGE;
     const currentPage = Number(page) || 1;
     const perPage = Number(itemsPerPage);
-    const startIdx = (currentPage - 1 ) * perPage;
-   
-    const products = await props.fetchData();
+    const startIdx = (currentPage - 1) * perPage;
 
-     const paginatedProducts = products.slice(startIdx , startIdx + perPage);
+    let products = [];
 
+    try {
+        products = await props.fetchData();
+    } catch {
+        return <div>{props.errorMessage}</div>;
+    }
+
+    const paginatedProducts = products.slice(startIdx, startIdx + perPage);
 
     return (
         <div>
             <ProductsSection
                 title={props.pageTitle}
-                viewAllButton={{ text: 'На головну', href: '/migrations' }}
+                viewAllButton={{ text: 'На головну', href: '/' }}
                 products={paginatedProducts}
             />
         </div>
