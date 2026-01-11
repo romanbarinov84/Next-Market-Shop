a5d932dabd554cddfc3c89b620f8103c68420e25
Roman Meinl <roman-meinl@ukr.net>
2026-01-11 13:10:59 +0200

    11.01 create config.ts && paginatedProducts in GenericProductListPage.tsx

---
+++ config/config.ts
@@ -0,0 +1,4 @@
+
+export const CONFIG = {
+    ITEMS_PER_PAGE: 4,
+}
--- src/app/(products)/GenericProductListPage.tsx
+++ src/app/(products)/GenericProductListPage.tsx
@@ -1,6 +1,7 @@
 
 import { GenericListPageProps } from '@/src/types/GenericProductListPage';
 import ProductsSection from './ProductsSection';
+import { CONFIG } from '@/config/config';
 
 const GenericProductListPage = async ({
     searchParams,
@@ -9,14 +10,25 @@ const GenericProductListPage = async ({
     searchParams: Promise<{ page?: string; itemsPerPage?: string }>;
     props: GenericListPageProps;
 }) => {
+
+    const params = await searchParams;
+    const page = params?.page;
+    const itemsPerPage = params?.itemsPerPage || CONFIG.ITEMS_PER_PAGE;
+    const currentPage = Number(page) || 1;
+    const perPage = Number(itemsPerPage);
+    const startIdx = (currentPage - 1 ) * perPage;
+   
     const products = await props.fetchData();
+
+     const paginatedProducts = products.slice(startIdx , startIdx + perPage);
+
 
     return (
         <div>
             <ProductsSection
                 title={props.pageTitle}
                 viewAllButton={{ text: 'На головну', href: '/migrations' }}
-                products={products}
+                products={paginatedProducts}
             />
         </div>
     );
