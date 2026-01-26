bb522615e8e6da5715d54acfa2809aa295b468c6
Roman Meinl <roman-meinl@ukr.net>
2026-01-26 10:34:58 +0200

    26.01 saving catalog changes in data base done

---
+++ src/app/(catalog)/catalog/CatalogPage.tsx
@@ -0,0 +1,238 @@
+'use client';
+
+import { CatalogProps } from '@/src/types/catalog';
+import { useEffect, useState } from 'react';
+import GridCategoryBlock from '../GridCategoryBlock';
+import CatalogLoading from '@/src/components/loading/CatalogLoader';
+
+
+
+const CatalogPage = () => {
+    const [categories, setCategories] = useState<CatalogProps[]>([]);
+    const [isEditing, setIsEditing] = useState(false);
+    const [draggableCategory, setDraggableCategory] =
+        useState<CatalogProps | null>(null);
+    const [err, setErr] = useState<string | null>(null);
+    const [isLoading, setIsLoading] = useState(true);
+    const [hoveredCategoryId, setHoveredCategoryId] = useState<string | null>(
+        null,
+    );
+    const isAdmin = true;
+
+    const fetchCategories = async () => {
+        setIsLoading(true);
+        try {
+            const response = await fetch('api/catalog');
+            if (!response.ok) {
+                throw new Error(`Ошибка сервера : ${response.status}`);
+            }
+            const data: CatalogProps[] = await response.json();
+
+            setCategories(data.sort((a, b) => a.order - b.order));
+        } catch (error) {
+            console.error(`Неудалось получить категории:`, error);
+            setErr('Неудалось загрузить категории');
+        } finally {
+            setIsLoading(false);
+        }
+    };
+
+    useEffect(() => {
+        fetchCategories();
+    }, []);
+
+    if (isLoading) {
+        return <CatalogLoading/>;
+    }
+
+    if (err) {
+       throw err
+    }
+
+    if (!categories.length) {
+        return (
+            <div className="text-center text-orange-600">
+                Категорий каталога не найденно
+            </div>
+        );
+    }
+
+    const updateOrderInDB = async () => {
+        try {
+             const response = await fetch("api/catalog", {
+                method:"POST",
+                headers:{
+                    "Content-Type":"application/json",
+
+                },
+                body: JSON.stringify(
+                    categories.map((category , index) => ({
+                        _id:category._id,
+                        order:index + 1,
+                        title:category.title,
+                        img:category.img,
+                        colSpan:category.colSpan,
+                        tabletColSpan:category.tabletColSpan,
+                        mobileColSpan:category.mobileColSpan,
+                    }))
+                ),
+             });
+
+           if(!response.ok){
+            throw new Error("Ошибка при обновлении порядка");
+           }
+
+           const result = await response.json();
+
+           if(result.success){
+            console.log("Порядок успешно обновлен в базе данных");
+           }
+
+        } catch (error) {
+            console.error(error)
+            setErr("Ошибка при сохранении порядка");
+        }
+    }
+
+    const handleToggleEditing = async () => {
+          
+        if(isEditing){
+            await updateOrderInDB();
+        }
+
+
+        setIsEditing(!isEditing);
+    };
+
+    const resetLayOut = () => {
+        fetchCategories();
+    };
+
+    const handleDragStart = (category: CatalogProps) => {
+        if (isEditing) {
+            setDraggableCategory(category);
+        }
+    };
+
+    const handleDragOver = (e: React.DragEvent, categoryId: string) => {
+        e.preventDefault();
+        if (draggableCategory && draggableCategory._id !== categoryId) {
+            setHoveredCategoryId(categoryId);
+        }
+    };
+
+    const handleDragLeave = () => {
+        setHoveredCategoryId(null)
+    }
+
+    const handleDrop = (e: React.DragEvent, targetCategoryId:string) => {
+         e.preventDefault();
+
+         if(!isEditing || !draggableCategory){
+            return
+         }
+
+         setCategories((prevCategories) => {
+            const draggedIndex = prevCategories.findIndex((c) => c._id === draggableCategory._id);
+
+            const targetIndex = prevCategories.findIndex((c) => c._id === targetCategoryId);
+
+            if(draggedIndex === -1 || targetIndex === -1) return prevCategories;
+
+            const newCategories = [...prevCategories];
+
+            const draggedItem = newCategories[draggedIndex];
+            const targetItem = newCategories[targetIndex];
+
+            const draggedSizes = {
+                mobileCoSpan:draggedItem.mobileColSpan,
+                tabletColSpan:draggedItem.tabletColSpan,
+                colSpan:draggedItem.colSpan,
+            }
+            const targetSizes = {
+                mobileCoSpan:targetItem.mobileColSpan,
+                tabletColSpan:targetItem.tabletColSpan,
+                colSpan:targetItem.colSpan,
+            };
+
+            newCategories[draggedIndex] = {...targetItem, ...draggedSizes};
+            newCategories[targetIndex] = {...draggedItem, ...targetSizes};
+
+            return newCategories;
+         } );
+
+         setDraggableCategory(null);
+         setHoveredCategoryId(null);
+    }
+
+    return (
+        <section className="w-full px-4 sm:px-6 lg:px-8 xl:px-[max(12px,calc((100%-1208px)/2))]  flex flex-col justify-center  py-10">
+            {isAdmin && (
+                <div className="flex flex-col sm:flex-row justify-end gap-3 mt-4">
+                    {/* Edit / Update */}
+                    <button
+                        onClick={handleToggleEditing}
+                        className="  flex items-center justify-center h-10 w-full sm:w-1/4
+      
+        rounded-lg px-4  font-medium  border border-(--color-primary)  text-(--color-primary)
+       transition-all duration-300  cursor-pointer select-none    hover:bg-orange-300   hover:text-white  hover:border-transparent  active:scale-95 active:shadow-(--shadow-button-active)"
+                    >
+                        {isEditing ? 'Оновити' : 'Змінити'}
+                    </button>
+
+                    {/* Reset */}
+                    {isEditing && (
+                        <button
+                            onClick={resetLayOut}
+                            className="relative overflow-hidden flex items-center justify-center
+           h-10  w-full sm:w-1/4  px-4 rounded-lg font-semibold text-white  bg-linear-to-r from-orange-400 via-orange-500 to-orange-600 shadow-md not-first:transition-all duration-300 hover:shadow-lg hover:brightness-110 active:scale-95 active:shadow-inner
+            focus:outline-none  focus:ring-2 focus:ring-orange-400  
+        "
+                        >
+                            Скинути
+                        </button>
+                    )}
+                </div>
+            )}
+
+            <h1 className="text-3xl sm:text-4xl font-bold text-gray-800 mb-8 xl:mb-10 flex flex-row xl:text-15">
+                Каталог
+            </h1>
+            <div className="w-full  bg-white/70 backdrop-blur-md rounded-2xl shadow-lg shadow-black/10 p-4 md:p-6 xl:p-8">
+                <div
+                    className="grid grid-cols-2  md:grid-cols-3 xl:grid-cols-4 gap-4 
+            md:gap-6 xl:gap-8"
+                >
+                    {categories.map((category) => (
+                        <div
+                            key={category._id}
+                            className={`${category.mobileColSpan} ${category.tabletColSpan} ${category.colSpan} bg-gray-200 rounded overflow-hidden min-h-50 h-full   ${isEditing ? '  border-dashed border-gray-300' : ''}  ${
+                                hoveredCategoryId === category._id ? "bg-green-200" : ""}`}
+                            onDragOver={(e) => handleDragOver(e, category._id)}
+                            onDragLeave={handleDragLeave}
+                            onDrop={(e) => handleDrop(e , category._id)}
+                        > 
+                            <div
+                                className={`h-full w-full  ${
+                                    draggableCategory?._id === category._id
+                                        ? 'opacity-50'
+                                        : ''
+                                }`}
+                                draggable={isEditing}
+                                onDragStart={() => handleDragStart(category)}
+                            >
+                                <GridCategoryBlock
+                                    id={category.id}
+                                    title={category.title}
+                                    img={category.img}
+                                />
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default CatalogPage;
