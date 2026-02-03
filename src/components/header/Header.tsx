9ebc7100da4cc5fd71956f4b4dc6e7977060b235
Roman Meinl <roman-meinl@ukr.net>
2026-02-03 11:06:15 +0200

    03.02 Header.tsx/create category.map()

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,23 +1,70 @@
-import HeaderUserBlock from "./UserBlock"
-import LogoBlock from "./LogoBlock"
-import SearchBlock from "./SearchBlock"
+'use client';
 
-
+import HeaderUserBlock from './UserBlock';
+import LogoBlock from './LogoBlock';
+import SearchBlock from './SearchBlock';
+import { useState } from 'react';
+import Link from 'next/link';
+import { Category } from '@/src/types/categories';
+import GlobalLoader from '../loading/GlobalLoader';
 
 function Header() {
-  return (
-    <header className="w-full bg-white md:shadow-(--shadow-default) items-center flex flex-col md:flex-row md:gap-y-5 xl:gap-y-7 md:gap-10 md:p-2 justify-center relative z-90">
-        <div className="flex flex-row gap-4 xl:gap-10 py-2 px-4 items-center shadow-md md:shadow-none">
-          <LogoBlock/>
-        <SearchBlock/>  
-        </div>
-        
-        <nav aria-label="Загальне меню">
-          <HeaderUserBlock/>  
-        </nav>
-        
-    </header>
-  )
+    const [isCatalogOpen, setIsCatalogOpen] = useState(false);
+    const [isLoading, setIsLoading] = useState(false);
+    const [categories, setCategories] = useState<Category[]>([]);
+
+    const fetchCategories = async () => {
+        if (categories.length > 0) return;
+
+        try {
+            const response = await fetch('/api/catalog/');
+            const data = await response.json();
+            setCategories(data);
+        } catch (error) {
+            console.error('Ошибка загрузки категории', error);
+        } finally {
+            setIsLoading(false);
+        }
+    };
+
+    const openMenu = () => {
+        setIsCatalogOpen(true);
+        fetchCategories();
+    };
+
+    return (
+        <header className="w-full bg-white md:shadow-(--shadow-default) items-center flex flex-col md:flex-row md:gap-y-5 xl:gap-y-7 md:gap-10 md:p-2 justify-center relative z-90">
+            <div className="flex flex-row gap-4 xl:gap-10 py-2 px-4 items-center shadow-md md:shadow-none">
+                <LogoBlock />
+                <div className="flex items-center" onMouseEnter={openMenu}>
+                    <SearchBlock />
+                </div>
+            </div>
+
+            {isCatalogOpen && (
+                <div className="hidden md:block absolute top-full left-0 w-full bg-white shadow-(--shadow-catalog-menu) z-50">
+                    <div className="mx-auto px-4 py-3">
+                        {isLoading && <GlobalLoader />}
+                        <div className="grid grid-cols-2 xl:grid-cols-4 gap-8">
+                            {categories.map((category) => (
+                                <Link
+                                    key={category.id}
+                                    href={`/category/${category.id}`}
+                                    className="block px-4 py-2 text-[#414141] hover:text-[#ff6633] font-bold duration-300"
+                                >
+                                    {category.title}
+                                </Link>
+                            ))}
+                        </div>
+                    </div>
+                </div>
+            )}
+
+            <nav aria-label="Загальне меню">
+                <HeaderUserBlock />
+            </nav>
+        </header>
+    );
 }
 
-export default Header
+export default Header;
