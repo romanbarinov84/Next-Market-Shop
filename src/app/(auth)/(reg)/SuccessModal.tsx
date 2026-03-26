c854560129ac7dc67361802437f33123e1326d35
Roman Meinl <roman-meinl@ukr.net>
2026-03-26 12:28:30 +0200

    26.03 start part 30

--- src/app/(auth)/(reg)/SuccessModal.tsx
+++ src/app/(auth)/(reg)/SuccessModal.tsx
@@ -6,7 +6,7 @@ const SuccessModal = () => {
 
   useEffect(() => {
     const timer = setTimeout(() => {
-      router.push("login");
+      router.replace("login");
     }, 3000);
 
     return () => clearTimeout(timer);
