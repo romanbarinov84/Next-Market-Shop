7a042e58410d89a3039d11a735c97eeb57e1a1b0
Roman <roman-meinl@ukr.net>
2026-05-19 13:51:53 +0200

    19.05 end part 49

--- UTILS/pathTranslations.ts
+++ UTILS/pathTranslations.ts
@@ -25,5 +25,5 @@
     "products":"Продукти",
     "add-product":"Додати продукт",
     "products-list":"Список товаров",
-    "edit-products":"Редактировать товар",
+    "edit-product":"Редактировать товар",
 }
--- src/app/(admin)/administrator/products/page.tsx
+++ src/app/(admin)/administrator/products/page.tsx
@@ -1,21 +1,15 @@
 
+import { buttonStyles } from "@/src/app/(auth)/styles";
 import Link from "next/link";
-import { buttonStyles } from "../../(auth)/(reg)/_components/styles";
 
-
-
-const AdminPanel = () => {
+const AdminProducts = () => {
   return (
     <div className="p-6">
-      <h1 className="text-2xl text-main-text font-bold mb-6">Панель управления</h1>
+      <h1 className="text-2xl text-main-text font-bold mb-6">
+        Панель управления
+      </h1>
 
       <div className="grid gap-4">
-        <Link
-          href="/administrator/users-list"
-          className={`${buttonStyles.active} [&&]:justify-start px-4 py-2 w-full md:w-1/2`}
-        >
-          Управление пользователями
-        </Link>
         <Link
           href="/administrator/products/add-product"
           className={`${buttonStyles.active} [&&]:justify-start px-4 py-2 w-full md:w-1/2`}
@@ -29,7 +23,7 @@ const AdminPanel = () => {
           Список товаров
         </Link>
         <Link
-          href="/administrator/products/edit-product"
+          href="/administrator/products/products-list"
           className={`${buttonStyles.active} [&&]:justify-start px-4 py-2 w-full md:w-1/2`}
         >
           Редактирование товара
@@ -39,4 +33,4 @@ const AdminPanel = () => {
   );
 };
 
-export default AdminPanel;
+export default AdminProducts;
